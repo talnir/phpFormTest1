76bc0743c80b5ed2f7c569c57a7be475d360284d
talnir <tal.talnir@gmail.com>
2014-02-10 20:57:11 +0200

    Router GUI frames

---
+++ dynaform/custom.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var str_wps_name_long = "Quick Secure Setup";
+var str_wps_name_short = "QSS";
+var wlan_wds = 1;
+var our_web_site = "www.tp-link.com"
+var wireless_ssid_prefix = "TP-LINK"
